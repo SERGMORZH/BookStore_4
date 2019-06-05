688887997afb675d592e51c63f17582c04a2b4be
egormozzharov <em@aperitas.com>
2019-06-05 10:49:44 +0300

    first commit

---
+++ BookStore_4/Global.asax.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Web.Http;
+using BookStore_4.Models;
+using System.Data.Entity;
+
+namespace BookStore_4
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            Database.SetInitializer(new BookDbInitializer());
+            // Code that runs on application startup
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);            
+        }
+    }
+}
